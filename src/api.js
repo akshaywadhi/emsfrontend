53c2c59cc82db8b60e5320642afaf8cee12396ff
Akshay Wadhi <168399945+akshaywadhi@users.noreply.github.com>
2025-09-29 21:38:33 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   //baseURL: "http://localhost:5000/api",
-  baseURL: "https://emsbackend-gizp.onrender.com/api",
+  baseURL: "https://emsbackend-gizp.onrender.com",
 });
 
 

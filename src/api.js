b69b63be69bc6c47b3696073f9ceecd67a2ff763
Akshay Wadhi <168399945+akshaywadhi@users.noreply.github.com>
2025-11-04 15:09:05 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -11,18 +11,17 @@ api.interceptors.request.use((config) => {
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-  console.log(`API Request: ${config.method?.toUpperCase()} ${config.baseURL}${config.url}`);
-  console.log("Request headers:", config.headers);
+
   return config;
 });
 
 api.interceptors.response.use(
   (response) => {
-    console.log(`API Response: ${response.status} ${response.config.url}`);
+  
     return response;
   },
   (error) => {
-    console.error(`API Error: ${error.response?.status} ${error.config?.url}`, error.response?.data);
+
     return Promise.reject(error);
   }
 );
